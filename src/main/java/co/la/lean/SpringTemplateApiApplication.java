ccd8bf12c0dd42eb54d03c1c861fff0f33cba016
Xieng <sp.xayyalath@gmail.com>
2025-05-07 13:09:59 +0700

    [initialize] Spring Boot project with Maven wrapper and basic configuration

---
+++ src/main/java/co/la/lean/SpringTemplateApiApplication.java
@@ -0,0 +1,13 @@
+package co.la.lean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTemplateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/la/lean/SpringTemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.la.lean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
